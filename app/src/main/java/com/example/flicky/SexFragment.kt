ae601ba5216b60b4b13ba363ae23ae78795dc6cd
remboshelby_work <happinessss57@gmail.com>
2019-06-19 16:09:37 +0300

    Initial commit

--- app/src/main/java/com/example/flicky/SexFragment.kt
+++ app/src/main/java/com/example/flicky/SexFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.common.base.BaseFragment
+import com.example.myinstagram.R
 import kotlinx.android.synthetic.main.sex_layout.*
 
 class SexFragment: BaseFragment(), CustomToolbar.CustomToolbarListener {
@@ -18,7 +19,7 @@ class SexFragment: BaseFragment(), CustomToolbar.CustomToolbarListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         customToolbar.setCustomListener(this)
-
+        val ttt ="fdsfsfd"
         btn_next.setOnClickListener({
             getRoot().pushFragment(EmailFragment(), true)
         })
@@ -27,8 +28,8 @@ class SexFragment: BaseFragment(), CustomToolbar.CustomToolbarListener {
             btn_m.setBackgroundResource(R.drawable.btn_not_checked)
         })
         btn_m.setOnClickListener({
-            btn_m.setBackgroundResource(R.drawable.btn_checked)
-            btn_w.setBackgroundResource(R.drawable.btn_not_checked)
+            btn_w.setBackgroundResource(R.drawable.btn_checked)
+            btn_m.setBackgroundResource(R.drawable.btn_not_checked)
         })
 
     }
